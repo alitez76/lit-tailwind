90aba3508db04d750103449a3d88a092b80b70ad
alitez76 <ali.tezerjani@outlook.com>
2022-03-31 12:45:04 -0700

    testing shadow-dom with vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,19 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	build: {
-		lib: {
-			entry: "src/my-element.ts",
-			formats: ["es"],
-		},
-		rollupOptions: {
-			external: /^lit/,
-		},
-	},
+  test: {
+    globals: true,
+    environment: "happy-dom",
+  },
+  build: {
+    lib: {
+      entry: "src/my-element.ts",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: /^lit/,
+    },
+  },
 });
